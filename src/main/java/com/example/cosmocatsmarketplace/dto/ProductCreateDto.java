9357cf0d7f5813377e11a536e6784e82fef59155
Andriy Plaksenkov <plaksenkov.andriy@lll.kpi.ua>
2024-11-02 19:16:39 +0200

    added Response, Update, Create DTO's for Product

--- src/main/java/com/example/cosmocatsmarketplace/dto/ProductCreateDto.java
+++ src/main/java/com/example/cosmocatsmarketplace/dto/ProductCreateDto.java
@@ -3,13 +3,11 @@ package com.example.cosmocatsmarketplace.dto;
 import com.example.cosmocatsmarketplace.domain.Category;
 import lombok.Data;
 
-
 import java.util.List;
 import java.util.UUID;
 
 @Data
-public class ProductDto {
-    private UUID id;
+public class ProductCreateDto {
     private String name;
     private String description;
     private Integer price;
---
+++ src/main/java/com/example/cosmocatsmarketplace/dto/ProductResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.cosmocatsmarketplace.dto;
+
+public class ProductResponseDto {
+}
