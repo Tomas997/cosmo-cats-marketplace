7453908dd0c96b3999392a95239c13a33d394542
Andriy Plaksenkov <plaksenkov.andriy@lll.kpi.ua>
2024-11-09 17:36:07 +0200

    added @GroupSequence for DTOs

--- src/main/java/com/example/cosmocatsmarketplace/dto/product/ProductUpdateDto.java
+++ src/main/java/com/example/cosmocatsmarketplace/dto/product/ProductUpdateDto.java
@@ -1,7 +1,9 @@
 package com.example.cosmocatsmarketplace.dto.product;
 
 import com.example.cosmocatsmarketplace.domain.Category;
+import com.example.cosmocatsmarketplace.dto.validation.Extended;
 import com.example.cosmocatsmarketplace.validator.ValidSpaceDescription;
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.extern.jackson.Jacksonized;
@@ -10,13 +12,14 @@ import lombok.extern.jackson.Jacksonized;
 @Value
 @Builder
 @Jacksonized
+@GroupSequence({ProductCreateDto.class, Extended.class})
 public class ProductUpdateDto {
     @NotBlank(message = "Name is mandatory")
     @Size(max = 100, message = "Name cannot exceed 100 characters")
     String name;
     @NotBlank(message = "Description is mandatory")
     @Size(max = 255, message = "Description cannot exceed 255 characters")
-    @ValidSpaceDescription
+    @ValidSpaceDescription(groups = Extended.class)
     String description;
     @NotNull(message = "Price is mandatory")
     @Min(value = 1, message = "Price cannot be 0 or less")
